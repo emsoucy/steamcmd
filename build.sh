4bc09be21983b7707eaf37fa6676ee7061e3aa7e
Ethan Soucy <ethan.soucy@gmail.com>
2021-09-29 11:19:23 -0400

    Raise ulimit nofile and add langpack.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 # Vars
-CONTAINER=$(buildah from scratch)
+CONTAINER=$(buildah from --ulimit nofile=2048 scratch)
 MOUNTPOINT=$(buildah mount $CONTAINER)
 
 URL='https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
@@ -11,8 +11,9 @@ VER=$(egrep '^VERSION_ID' /etc/os-release | cut -d '=' -f2)
 
 # Install dependencies
 if command -v 'dnf' &> /dev/null; then
-  dnf install -y --installroot $MOUNTPOINT --releasever $VER coreutils\
-    glibc.i686 libstdc++.i686 --nodocs --setopt install_weak_deps=False
+  dnf install -y --installroot $MOUNTPOINT --releasever $VER\
+    coreutils glibc-langpack-en glibc.i686 libstdc++.i686\
+    --nodocs --refresh --setopt install_weak_deps=False
   dnf clean all -y --installroot $MOUNTPOINT --releasever $VER
 else
   echo "Build script requires dnf package manager. Exiting."; exit
@@ -32,7 +33,7 @@ wget -qO- $URL | tar xvzf - -C $MOUNTPOINT$STEAM
 chmod -R 700 $MOUNTPOINT/home/steam
 chown -R 1000:1000 $MOUNTPOINT/home/steam
 buildah run $CONTAINER -- sh\
- -c "$STEAM/steamcmd.sh +login anonymous validate +exit"
-buildah unmount $CONTAINER
+  -c "$STEAM/steamcmd.sh +login anonymous validate +exit"
 
-buildah commit --squash $CONTAINER docker.io/emsoucy/steamcmd 
+buildah unmount $CONTAINER
+buildah commit --squash $CONTAINER steamcmd 
